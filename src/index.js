dac7df6a694ecd474c9684f409fe5b2b7e2e667d
github username <github email address>
2021-01-28 19:31:43 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 let addToy = false;
+const addBtn = document.querySelector("#new-toy-btn");
+const toyFormContainer = document.querySelector(".container");
 
 document.addEventListener("DOMContentLoaded", () => {
-  const addBtn = document.querySelector("#new-toy-btn");
-  const toyFormContainer = document.querySelector(".container");
+  fetch("http://localhost:3000/toys")
+    .then(r => r.json())
+    .then(toys => {
+      let toysHTML = toys.map(function(toy){
+        return `  
+      <div class="card">
+        <h2>${toy.name}</h2>
+        <img src=${toy.image} class="toy-avatar" />
+        <p>${toy.likes} Likes </p>
+        <button class="like-btn">Like <3</button>
+      </div>`
+      })
+      document.querySelector("#toy-collection").innerHTML = toysHTML
+  })
+ 
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
